57a62baad1536968f187b4f29ae6e4789348fde3
innjie <innjie@naver.com>
2022-05-31 14:02:08 +0900

    [update] component lecture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
 import React, {Component} from "react";
 import './App.css';
 
+//componenet를 만드는 코드
+class Subject extends Component {
+    render() {
+        return (
+            <header>
+                <h1>WEB</h1>
+                word wide web!
+            </header>
+        );
+    }
+}
+class NSubject extends Component {
+    render() {
+        return (
+            <ul>
+                <li><a href = "1.html">HTML</a></li>
+                <li><a href = "2.html">HTML</a></li>
+                <li><a href = "3.html">HTML</a></li>
+            </ul>
+
+        );
+    }
+}
+class ASubject extends Component {
+    render() {
+        return (
+            <article>
+                <h2>HTML</h2>
+                HTML is HyperText Markup Language.
+            </article>
+        );
+    }
+}
 class App extends Component {
     render() {
         return (
             <div className="App">
-                Hello React
+                <Subject/>
+                <NSubject/>
+                <ASubject/>
             </div>
         );
     }
