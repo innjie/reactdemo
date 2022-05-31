1e2e2403474339b085ccfd908e627144c6f082e9
innjie <innjie@naver.com>
2022-05-31 14:51:12 +0900

    [update] component lecture

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import React, {Component} from "react";
 import './App.css';
+import TOC from "./component/TOC";
+import Content from "./component/Content";
+import Subject from "./component/Subject";
 
-//componenet를 만드는 코드
-class Subject extends Component {
-    render() {
-        return (
-            <header>
-                <h1>{this.props.title}</h1>
-                {this.props.sub}
-            </header>
-        );
-    }
-}
-class NSubject extends Component {
-    render() {
-        return (
-            <ul>
-                <li><a href = "1.html">HTML</a></li>
-                <li><a href = "2.html">HTML</a></li>
-                <li><a href = "3.html">HTML</a></li>
-            </ul>
-
-        );
-    }
-}
-class ASubject extends Component {
-    render() {
-        return (
-            <article>
-                <h2>{this.props.title}</h2>
-                {this.props.desc}
-            </article>
-        );
-    }
-}
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Subject title="WEB" sub="word wide web!"></Subject>
                 <Subject title="React" sub="For UI"></Subject>
-                <NSubject/>
-                <ASubject title="HTML" desc = "HTML is HyperText Markup Language."></ASubject>
+                <TOC/>
+                <Content title="HTML" desc = "HTML is HyperText Markup Language."></Content>
             </div>
         );
     }
---
+++ src/component/TOC.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class TOC extends Component {
+    render() {
+        return (
+            <ul>
+                <li><a href = "1.html">HTML</a></li>
+                <li><a href = "2.html">HTML</a></li>
+                <li><a href = "3.html">HTML</a></li>
+            </ul>
+
+        );
+    }
+}
+
+export default TOC;
